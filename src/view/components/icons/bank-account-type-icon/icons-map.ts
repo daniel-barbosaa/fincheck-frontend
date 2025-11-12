fd1628741ce85fc7026cdf528dc2ce89f3f90bd6
daniel-barbosaa <danimendes9728@gmail.com>
2025-11-11 22:08:48 -0300

    feat(dashboard): basic structure created

---
+++ src/view/components/icons/bank-account-type-icon/icons-map.ts
@@ -0,0 +1,9 @@
+import { CashIcon } from "./cash-icon";
+import { CheckingIcon } from "./checking-icon";
+import { InvestmentIcon } from "./investment-icon";
+
+export const iconsMap = {
+  checking: CheckingIcon,
+  investment: InvestmentIcon,
+  cash: CashIcon,
+};
--- src/view/pages/dashboard/index.tsx
+++ src/view/pages/dashboard/index.tsx
@@ -1,13 +1,23 @@
-import { useAuth } from "../../../app/hooks/use-auth";
-import { Button } from "../../components/button";
+import { Logo } from "../../components/logo";
+import { UserMenu } from "../../components/user-menu";
+import { Accounts } from "./components/accounts";
+import { Transactions } from "./components/transactions";
 
 export function Dashboard() {
-  const { signout } = useAuth();
-
   return (
-    <>
-      <h1>Dashboard page</h1>
-      <Button onClick={signout}>sair</Button>
-    </>
+    <div className=",md:pt-6 flex h-full w-full flex-col gap-4 p-4 md:p-8">
+      <header className="flex h-12 items-center justify-between">
+        <Logo className="h-6 text-teal-900" />
+        <UserMenu />
+      </header>
+      <main className="flex flex-1 flex-col gap-4 md:flex-row">
+        <section className="w-full md:max-w-1/2">
+          <Accounts />
+        </section>
+        <section className="w-full md:max-w-1/2">
+          <Transactions />
+        </section>
+      </main>
+    </div>
   );
 }
