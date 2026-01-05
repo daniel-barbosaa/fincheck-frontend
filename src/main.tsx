c1a0ca3c9066cf0b455947cdb112fd245573dadf
daniel-barbosaa <danimendes9728@gmail.com>
2026-01-05 20:40:33 -0300

    chore: Implements a feature to prevent dynamic import errors in Vite.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./global.css";
 import { App } from "./App";
 
+import("../src/app/utils/lazy-with-reload");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
